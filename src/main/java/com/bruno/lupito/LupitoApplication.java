fd90983bbc2ea55bd1157e8ef0957c6f64800a44
BrunoFhome <brunodevjunior2022@gmail.com>
2026-02-26 09:43:33 -0300

    projeto iniciado sem config

---
+++ src/main/java/com/bruno/lupito/LupitoApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.lupito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LupitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/lupito/LupitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.lupito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
