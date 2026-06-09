26a07fc7161c8458313fe42819538170830eb28f
BrunoFhome <brunodevjunior2022@gmail.com>
2026-06-09 16:20:50 -0300

    mudanca landing page

--- src/test/java/com/bruno/lupito/LupitoApplicationTests.java
+++ src/test/java/com/bruno/lupito/LupitoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.bruno.lupito;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LupitoApplicationTests {
 
 	@Test
